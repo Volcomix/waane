18453d6ce6ff54eebd36b9e9e7b996c43bd55a16
Volcomix <volcomix@gmail.com>
2019-02-03 17:58:24 +0100

    Split components to AddMenu and ContextMenu

--- src/components/AddMenu.vue
+++ src/components/AddMenu.vue
@@ -1,63 +1,20 @@
 <template>
-  <div
-    v-if="value"
-    class="scrim"
-    @contextmenu.prevent.stop
-    @mouseenter.once="translate"
-    @mousedown.self="hide"
-  >
-    <ul v-show="hasPosition" class="menu body2" :style="position">
-      Sloubi
-    </ul>
-  </div>
+  <ContextMenu :value="value" v-on="$listeners">
+    <li>Sloubi</li>
+    <li>Testouille</li>
+  </ContextMenu>
 </template>
 
 <script>
+import ContextMenu from './ContextMenu.vue'
+
 export default {
   name: 'AddMenu',
+  components: {
+    ContextMenu,
+  },
   props: {
     value: Boolean,
   },
-  data() {
-    return {
-      hasPosition: false,
-      x: 0,
-      y: 0,
-    }
-  },
-  computed: {
-    position() {
-      return {
-        top: `${this.y}px`,
-        left: `${this.x}px`,
-      }
-    },
-  },
-  methods: {
-    translate(event) {
-      this.hasPosition = true
-      this.x = event.clientX
-      this.y = event.clientY
-    },
-    hide() {
-      this.hasPosition = false
-      this.$emit('input', false)
-    },
-  },
 }
 </script>
-
-<style scoped>
-.scrim {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-}
-.menu {
-  position: absolute;
-  margin: 0;
-  padding: 8px 16px;
-  background-color: rgb(var(--background));
-  color: rgb(var(--on-background));
-}
-</style>
