38f60c41693cfba6eb9bc6601dc698a08a5eb970
Volcomix <Volcomix@users.noreply.github.com>
2019-12-09 20:02:09 +0100

    Configure linting for Vue app

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -132,7 +132,10 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'HelloWorld',
   props: {
-    msg: String,
+    msg: {
+      type: String,
+      required: true,
+    },
   },
 })
 </script>
