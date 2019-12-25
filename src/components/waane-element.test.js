4697de13f4fd2a6d2f5a42679a8cfff4d7abc18e
Volcomix <Volcomix@users.noreply.github.com>
2019-12-25 18:24:00 +0100

    Init WaaneElement tests

--- src/components/waane-element.test.js
+++ src/components/waane-element.test.js
@@ -1,11 +1,41 @@
 import { html } from './waane-element.js'
 
 describe('html', () => {
-  test.todo('returns a template')
+  let template
+
+  beforeEach(() => {
+    template = html`
+      <span>A template content</span>
+    `
+  })
+
+  it('returns a template', () => {
+    expect(template).toBeInstanceOf(HTMLTemplateElement)
+  })
+
+  it('stores the correct content inside the template', () => {
+    expect(template.content).toMatchSnapshot()
+  })
 })
 
 describe('WaaneElement', () => {
-  test.todo('memoizes template')
+  it.todo('initializes the shadow root')
 
-  test.todo('memoizes attributes')
+  it.todo('registers a _setter for each attribute')
+
+  it.todo('registers a property for each attribute')
+
+  it.todo('does not throw without template')
+
+  it.todo('does not throw without observedAttributes')
+
+  it.todo('memoizes template')
+
+  it.todo('memoizes observedAttributes')
+
+  it.todo('calls the associated _setter when an attribute changes')
+
+  it.todo('reflects from property to attribute')
+
+  it.todo('reflects from attribute to property')
 })
