e1c6fd15e9ea075b0a0321fb60acc5666c4b9980
Volcomix <Volcomix@users.noreply.github.com>
2019-12-26 14:15:25 +0100

    Refactor defining element in unit tests

--- src/components/waane-element.test.js
+++ src/components/waane-element.test.js
@@ -11,55 +11,51 @@ describe('html', () => {
   let templateHandle
 
   beforeEach(async () => {
-    templateHandle = await evaluateHandle(
-      ({ html }) => html`
+    templateHandle = await evaluateHandle(({ html }) => {
+      return html`
         <span>A content</span>
-      `,
-    )
+      `
+    })
   })
 
   it('returns a template', async () => {
-    const isTemplate = await templateHandle.evaluate(
-      template => template instanceof HTMLTemplateElement,
-    )
+    const isTemplate = await templateHandle.evaluate(template => {
+      return template instanceof HTMLTemplateElement
+    })
     expect(isTemplate).toBe(true)
   })
 
   it('stores the correct content inside the template', async () => {
-    const content = await templateHandle.evaluateHandle(
-      template => template.content,
-    )
-    await expect(content).toMatchElement('span', { text: 'A content' })
+    const contentHandle = await templateHandle.getProperty('content')
+    await expect(contentHandle).toMatchElement('span', { text: 'A content' })
   })
 })
 
 describe('WaaneElement', () => {
   it('has no shadow root without template', async () => {
-    const hasShadowRoot = await page.evaluate(({ WaaneElement }) => {
-      class NoTemplateElement extends WaaneElement {}
-
-      customElements.define('no-template-element', NoTemplateElement)
-      return !!document.createElement('no-template-element').shadowRoot
-    }, moduleHandle)
-
+    const tagName = await defineElement(({ WaaneElement }) => {
+      return class extends WaaneElement {}
+    })
+    const elementHandle = await createElement(tagName)
+    const hasShadowRoot = await elementHandle.evaluate(element => {
+      return !!element.shadowRoot
+    })
     expect(hasShadowRoot).toBe(false)
   })
 
   it('initializes the shadow root without observedAttributes', async () => {
-    const shadowRoot = await page.evaluateHandle(({ WaaneElement, html }) => {
-      class ShadowElement extends WaaneElement {
+    const tagName = await defineElement(({ WaaneElement, html }) => {
+      return class extends WaaneElement {
         static get template() {
           return html`
             <span>A shadow</span>
           `
         }
       }
-
-      customElements.define('shadow-element', ShadowElement)
-      return document.createElement('shadow-element').shadowRoot
-    }, moduleHandle)
-
-    await expect(shadowRoot).toMatchElement('span', { text: 'A shadow' })
+    })
+    const elementHandle = await createElement(tagName)
+    const shadowRootHandle = await elementHandle.getProperty('shadowRoot')
+    await expect(shadowRootHandle).toMatchElement('span', { text: 'A shadow' })
   })
 
   it('memoizes template', async () => {
@@ -97,10 +93,27 @@ describe('WaaneElement', () => {
   it.todo('reflects from attribute to property')
 })
 
-function evaluateHandle(pageFunction) {
-  return page.evaluateHandle(pageFunction, moduleHandle)
+async function evaluateHandle(pageFunction) {
+  return await page.evaluateHandle(pageFunction, moduleHandle)
 }
 
-function evaluate(pageFunction) {
-  return page.evaluate(pageFunction, moduleHandle)
+async function evaluate(pageFunction) {
+  return await page.evaluate(pageFunction, moduleHandle)
+}
+
+async function defineElement(pageFunction) {
+  const elementClassHandle = await evaluateHandle(pageFunction)
+
+  return await elementClassHandle.evaluate(elementClass => {
+    window.testTagNameId = (window.testTagNameId || 0) + 1
+    const tagName = `test-${testTagNameId}`
+    customElements.define(tagName, elementClass)
+    return tagName
+  })
+}
+
+async function createElement(tagName) {
+  return await page.evaluateHandle(tagName => {
+    return document.createElement(tagName)
+  }, tagName)
 }
