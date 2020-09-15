c8d9a95461d723a831b648a0106538976c4592ec
Volcomix <Volcomix@users.noreply.github.com>
2020-09-15 12:54:38 +0200

    Add typography test page

--- src/audio-node-editor/__tests__/audio-node-editor.js
+++ src/audio-node-editor/__tests__/audio-node-editor.js
@@ -9,9 +9,9 @@ test('has no node by default', () => {
 
 test('adds an oscillator node', () => {
   const audioNodeEditor = document.createElement('audio-node-editor')
-  const oscillatorMenuItem = /** @type {HTMLElement} */ (Array.from(
-    audioNodeEditor.shadowRoot.querySelectorAll('w-menu-item'),
-  ).find((menuItem) => menuItem.textContent === 'Oscillator'))
+  const oscillatorMenuItem = /** @type {HTMLElement} */ ([
+    ...audioNodeEditor.shadowRoot.querySelectorAll('w-menu-item'),
+  ].find((menuItem) => menuItem.textContent === 'Oscillator'))
   oscillatorMenuItem.click()
   const graphNode = audioNodeEditor.shadowRoot.querySelector('w-graph-node')
   expect(graphNode.textContent).toBe('Oscillator')
