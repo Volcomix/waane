258acca978b1c180220b405b5cd22ba13fc00bc5
Volcomix <Volcomix@users.noreply.github.com>
2020-09-16 17:30:07 +0200

    Improve typings when no observedAttributes

--- src/shared/core/element.js
+++ src/shared/core/element.js
@@ -24,7 +24,7 @@ export const css = String.raw
  */
 
 /**
- * @template {Object.<string, Number>} T
+ * @template {Object.<string, Number> | {}} T
  * @param {string} name
  * @param {string} template
  * @param {Setup<T>} setup
