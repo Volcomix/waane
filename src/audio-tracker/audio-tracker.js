fac6858df035b6693e06409284ca19988e396ac2
Sébastien Jalliffier Verne <Volcomix@users.noreply.github.com>
2020-10-29 21:10:00 +0100

    Display not implemented message

--- src/audio-tracker/audio-tracker.js
+++ src/audio-tracker/audio-tracker.js
@@ -1,3 +1,16 @@
-import { defineCustomElement } from '../shared/core/element.js'
+import { css, defineCustomElement, html } from '../shared/core/element.js'
+import typography from '../shared/core/typography.js'
 
-export default defineCustomElement('audio-tracker', {})
+export default defineCustomElement('audio-tracker', {
+  styles: css`
+    span {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: rgba(var(--color-on-background) / var(--text-medium-emphasis));
+      ${typography('body2')}
+    }
+  `,
+  template: html`<span>Not implemented</span>`,
+})
