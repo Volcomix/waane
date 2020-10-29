8ecd2f6e2041f5693cb52fb8395f94d594089f8c
Sébastien Jalliffier Verne <Volcomix@users.noreply.github.com>
2020-10-29 21:01:02 +0100

    Switch between tabs

--- src/waane-app.js
+++ src/waane-app.js
@@ -26,7 +26,37 @@ export default defineCustomElement('waane-app', {
       <w-tab active>Nodes</w-tab>
     </header>
     <main>
+      <audio-tracker hidden></audio-tracker>
       <audio-node-editor></audio-node-editor>
     </main>
   `,
+  setup({ host }) {
+    const [
+      tracksTab,
+      nodesTab,
+    ] = /** @type {NodeListOf<import('./shared/base/tab.js').default>} */ (host.shadowRoot.querySelectorAll(
+      'w-tab',
+    ))
+
+    const audioTracker = /** @type {HTMLElement} */ (host.shadowRoot.querySelector(
+      'audio-tracker',
+    ))
+    const audioNodeEditor = /** @type {HTMLElement} */ (host.shadowRoot.querySelector(
+      'audio-node-editor',
+    ))
+
+    tracksTab.addEventListener('click', () => {
+      tracksTab.active = true
+      nodesTab.active = false
+      audioTracker.hidden = false
+      audioNodeEditor.hidden = true
+    })
+
+    nodesTab.addEventListener('click', () => {
+      tracksTab.active = false
+      nodesTab.active = true
+      audioTracker.hidden = true
+      audioNodeEditor.hidden = false
+    })
+  },
 })
