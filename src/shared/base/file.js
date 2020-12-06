df27460d06d4c31f2e6a9793efa717f12e186b2d
Volcomix <Volcomix@users.noreply.github.com>
2020-12-06 16:37:31 +0100

    Improve file input

--- src/shared/base/file.js
+++ src/shared/base/file.js
@@ -3,23 +3,62 @@ import { css, defineCustomElement, html } from '../core/element.js'
 export default defineCustomElement('w-file', {
   styles: css`
     :host {
+      width: 256px;
       display: flex;
       flex-direction: column;
-      align-items: center;
     }
 
     w-button {
       margin-bottom: 8px;
+      align-self: stretch;
       color: rgba(var(--color-on-surface) / var(--text-high-emphasis));
+      transition: color 200ms var(--easing-standard);
+    }
+
+    w-button.file-selected {
+      color: rgba(var(--color-on-surface) / var(--text-medium-emphasis));
     }
 
     w-icon {
       margin-right: 8px;
       font-size: 18px;
     }
+
+    span {
+      overflow: hidden;
+      text-align: center;
+      text-overflow: ellipsis;
+      color: rgba(var(--color-on-surface) / var(--text-medium-emphasis));
+      transition: color 200ms var(--easing-standard);
+    }
+
+    span.file-selected {
+      color: rgba(var(--color-on-surface) / var(--text-high-emphasis));
+    }
   `,
   template: html`
     <w-button><w-icon>cloud_upload</w-icon>Choose a file</w-button>
-    No file selected
+    <span>No file selected</span>
   `,
+  setup({ host }) {
+    const button = host.shadowRoot.querySelector('w-button')
+    const fileName = host.shadowRoot.querySelector('span')
+
+    const input = document.createElement('input')
+    input.type = 'file'
+
+    host.addEventListener('click', () => {
+      input.click()
+    })
+
+    input.addEventListener('change', async () => {
+      if (input.files.length !== 1) {
+        return
+      }
+      const file = input.files[0]
+      fileName.textContent = file.name
+      button.classList.add('file-selected')
+      fileName.classList.add('file-selected')
+    })
+  },
 })
