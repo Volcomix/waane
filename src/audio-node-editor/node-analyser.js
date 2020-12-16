274469961560e12f0de50bc5c5177a453001ed01
Volcomix <Volcomix@users.noreply.github.com>
2020-12-16 20:41:43 +0100

    Add color to analyzer rendering

--- src/audio-node-editor/node-analyser.js
+++ src/audio-node-editor/node-analyser.js
@@ -3,6 +3,35 @@ import { nextId } from '../shared/helpers/id.js'
 import useAudioContext from './use-audio-context.js'
 import { bindAudioInput, bindAudioOutput } from './use-audio-link.js'
 
+const period = 2
+
+const colors = [
+  [0, 0, 128, 0],
+  [0, 0, 128, 255],
+  [0, 128, 255, 255],
+  [0, 255, 0, 255],
+  [255, 255, 0, 255],
+  [255, 128, 0, 255],
+  [255, 0, 0, 255],
+]
+
+/**
+ * @param {number} value
+ */
+function getColor(value) {
+  const colorPosition = ((colors.length - 1) * value) / 256
+  const colorIndex = Math.floor(colorPosition)
+  const positionInColor = colorPosition - colorIndex
+  const [r1, g1, b1, a1] = colors[colorIndex]
+  const [r2, g2, b2, a2] = colors[colorIndex + 1]
+  return [
+    Math.min(r1, r2) + Math.abs(r1 - r2) * positionInColor,
+    Math.min(g1, g2) + Math.abs(g1 - g2) * positionInColor,
+    Math.min(b1, b2) + Math.abs(b1 - b2) * positionInColor,
+    Math.min(a1, a2) + Math.abs(a1 - a2) * positionInColor,
+  ]
+}
+
 export default defineCustomElement('node-analyser', {
   template: html`
     <w-graph-node>
@@ -43,10 +72,11 @@ export default defineCustomElement('node-analyser', {
       const newColumn = canvasContext.getImageData(x, 0, 1, canvas.height)
       for (let i = 0; i < frequencyData.length; i++) {
         const y = 4 * (frequencyData.length - i - 1)
-        newColumn.data[y] = frequencyData[i]
-        newColumn.data[y + 1] = frequencyData[i]
-        newColumn.data[y + 2] = frequencyData[i]
-        newColumn.data[y + 3] = frequencyData[i]
+        const [r, g, b, a] = getColor(frequencyData[i])
+        newColumn.data[y] = r
+        newColumn.data[y + 1] = g
+        newColumn.data[y + 2] = b
+        newColumn.data[y + 3] = a
       }
       canvasContext.putImageData(newColumn, x, 0)
     }
@@ -68,7 +98,7 @@ export default defineCustomElement('node-analyser', {
       const waaneApp = analyserWindow.document.body.querySelector('waane-app')
       const audioAnalyser = waaneApp.shadowRoot.querySelector('audio-analyser')
       canvas = audioAnalyser.shadowRoot.querySelector('canvas')
-      canvas.width = 2 * 60
+      canvas.width = period * 60
       canvas.height = analyser.frequencyBinCount
       canvasContext = canvas.getContext('2d')
 
